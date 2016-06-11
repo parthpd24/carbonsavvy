75291a1ef1de350f2d6530629d04a8328416b9d9
Ahan Raina <addyraina02@gmail.com>
2016-06-11 19:19:50 -0400

    Added electricity code.

--- transportation.js
+++ transportation.js
@@ -9,3 +9,40 @@ function Transportation_Footprint(gasmileage, miles_per_year) {
 	return Footprint_Transport;
 
 }
+
+function CalcElectricFootprint( coalFoot,  natGasFoot,  hydElecFoot,  oilFoot) //Final Equation for electricity carbon footprint
+{
+    var FinalElecFootprint = 0;
+    var TotalElecFootprint = coalFoot + natGasFoot + hydElecFoot + oilFoot;
+
+    FinalElecFootprint = TotalElecFootprint;
+    return FinalElecFootprint;
+}
+
+function CalcCoalFootprint(coalkWh) //Convert Coal KiloWatt Hours into Carbon Data
+{
+	var coalOutput = coalkWh * 1945.00464 * 0.2552;
+
+    return coalOutput;
+}
+function CalcNatGasFootPrint(natGaskWh) //Convert Natural Gas KiloWatt Hours into Carbon Data
+{
+     var natGasOutput = natGaskWh * 0.003715182 * 0.2001;
+
+    return natGasOutput;
+}
+function CalcHydElecFootPrint(hydEleckWh) //Convert Hydroelectricity KiloWatt Hours into Carbon Data
+{
+      var hydElecOutput = hydEleckWh * 0.000011 * 0.0005;
+
+    return hydElecOutput;
+}
+function CalcOilFootprint(oilkWh) //Convert Oil KiloWatt Hours into Carbon Data
+{
+      var oilOutput = oilkWh * 0.0026 * 0.008759688;
+
+    return oilOutput;
+}
+
+var PrafulsLoveForTya = true;
+
