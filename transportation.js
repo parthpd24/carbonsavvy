574fd8152370f1728c3b196b73c8b67adb30dd9e
Ahan Raina <addyraina02@gmail.com>
2016-06-11 18:44:38 -0400

    finished function

--- transportation.js
+++ transportation.js
@@ -2,4 +2,10 @@
 var gasmileage = 0;
 var miles_per_year = 0;
 
-function Transportation_Footprint(gasmileage, miles_per_year)
+function Transportation_Footprint(gasmileage, miles_per_year) {
+	var g = 1/gasmileage;
+	var GalYear = g*miles_per_year;
+	var Footprint_Transport = GoalYear(0.00878);
+	return Footprint_Transport;
+
+}
