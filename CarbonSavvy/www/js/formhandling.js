00542a4b6604f5f4790f3c6b160c39cd2d8ddf62
parthpd24 <parthpd24@gmail.com>
2016-06-11 20:45:29 -0400

    finished forms and getter functions

--- CarbonSavvy/www/js/formhandling.js
+++ CarbonSavvy/www/js/formhandling.js
@@ -6,3 +6,34 @@ namefunc(form) {
 nameoutput() {
     document.getElementById(nameplace).innerHTML = "Hi there, " + name + "!";
 }
+
+registerInfo(form) {
+    var kwh = form.kwh.value;
+    var zipcode = form.zipcode.value;
+    var mileage = form.mileage.value;
+    for (var i = 0; i < document.getElementsByName('radio').length; i++) {
+        if(document.getElementsByName('radio')[i].checked) {
+            var diet = document.getElementsByName('radio')[i].value;
+        }
+    }
+}
+
+returnkwh() {
+    return kwh;
+}
+
+returnzipcode() {
+    return zipcode;
+}
+
+returnmileage() {
+    return mileage;
+}
+
+returndiet() {
+    return diet;
+}
+
+returnname() {
+    return name;
+}
