56ae3730c444a682b576732480d87a5c9a9bb36b
ahanraina <addyraina02@gmail.com>
2018-04-21 19:22:26 -0400

    carbonsavvy impacts

---
+++ html5usmapv3.8/mapdata.js
@@ -0,0 +1,383 @@
+var simplemaps_usmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "650", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    
+    //Label defaults
+    label_color: "#002767",
+    hide_labels: "no",
+    border_color: "white",
+    
+    //State defaults
+    state_description: "Click to learn the impacts of climate change on your state.",
+    state_color: "#80dd99",
+    state_hover_color: "#038EEB",
+    state_url: "http://simplemaps.com",
+    all_states_inactive: "no",
+    
+    //Location defaults
+    location_description: "Field Office",
+    location_color: "#ff46a2",
+    location_pulse: "yes",
+    location_pulse_size: "3",
+    location_opacity: ".8",
+    location_url: "http://simplemaps.com",
+    all_states_zoomable: "yes",
+    location_size: "40",
+    location_type: "marker",
+    all_locations_inactive: "no",
+    url_new_tab: "yes",
+    auto_load: "yes",
+   
+    //Zoom settings
+    zoom: "yes",
+    initial_zoom: "-1",
+    initial_zoom_solo: "yes",
+    
+    //Advanced settings
+    div: "map",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: "",
+    label_hover_color: "",
+    label_size: "",
+    label_font: "",
+    popups: "detect"
+  },
+  state_specific: {
+    HI: {
+      name: "Hawaii",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-hi.pdf"
+    },
+    AK: {
+      name: "Alaska",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-ak.pdf"
+    },
+    FL: {
+      name: "Florida",
+      inactive: "no",
+      hide: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-fl.pdf"
+    },
+    NH: {
+      name: "New Hampshire",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-nh.pdf"
+    },
+    VT: {
+      name: "Vermont",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-vt.pdf"
+    },
+    ME: {
+      name: "Maine",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-me.pdf"
+    },
+    RI: {
+      name: "Rhode Island",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-ri.pdf"
+    },
+    NY: {
+      name: "New York",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-ny.pdf"
+    },
+    PA: {
+      name: "Pennsylvania",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-pa.pdf"
+    },
+    NJ: {
+      name: "New Jersey",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-nj.pdf"
+    },
+    DE: {
+      name: "Delaware",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-de.pdf"
+    },
+    MD: {
+      name: "Maryland",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-md.pdf"
+    },
+    VA: {
+      name: "Virginia",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-va.pdf"
+    },
+    WV: {
+      name: "West Virginia",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-wv.pdf"
+    },
+    OH: {
+      name: "Ohio",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-oh.pdf"
+    },
+    IN: {
+      name: "Indiana",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-in.pdf"
+    },
+    IL: {
+      name: "Illinois",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-il.pdf"
+    },
+    CT: {
+      name: "Connecticut",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-ct.pdf"
+    },
+    WI: {
+      name: "Wisconsin",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-wi.pdf"
+    },
+    NC: {
+      name: "North Carolina",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-nc.pdf"
+    },
+    DC: {
+      name: "District of Columbia",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-11/documents/climate-change-dc.pdf"
+    },
+    MA: {
+      name: "Massachusetts",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-ma.pdf"
+    },
+    TN: {
+      name: "Tennessee",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-tn.pdf"
+    },
+    AR: {
+      name: "Arkansas",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-ar.pdf"
+    },
+    MO: {
+      name: "Missouri",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-mo.pdf"
+    },
+    GA: {
+      name: "Georgia",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-ga.pdf"
+    },
+    SC: {
+      name: "South Carolina",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-sc.pdf"
+    },
+    KY: {
+      name: "Kentucky",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-ky.pdf"
+    },
+    AL: {
+      name: "Alabama",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-al.pdf"
+    },
+    LA: {
+      name: "Louisiana",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-la.pdf"
+    },
+    MS: {
+      name: "Mississippi",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-ms.pdf"
+    },
+    IA: {
+      name: "Iowa",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-ia.pdf"
+    },
+    MN: {
+      name: "Minnesota",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-mn.pdf"
+    },
+    OK: {
+      name: "Oklahoma",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-ok.pdf"
+    },
+    TX: {
+      name: "Texas",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-tx.pdf"
+    },
+    NM: {
+      name: "New Mexico",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-nm.pdf"
+    },
+    KS: {
+      name: "Kansas",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-ks.pdf"
+    },
+    NE: {
+      name: "Nebraska",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-ne.pdf"
+    },
+    SD: {
+      name: "South Dakota",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-sd.pdf"
+    },
+    ND: {
+      name: "North Dakota",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-nd.pdf"
+    },
+    WY: {
+      name: "Wyoming",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-wy.pdf"
+    },
+    MT: {
+      name: "Montana",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-mt.pdf"
+    },
+    CO: {
+      name: "Colorado",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-co.pdf"
+    },
+    UT: {
+      name: "Utah",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-ut.pdf"
+    },
+    AZ: {
+      name: "Arizona",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-az.pdf"
+    },
+    NV: {
+      name: "Nevada",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-nv.pdf"
+    },
+    OR: {
+      name: "Oregon",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-or.pdf"
+    },
+    WA: {
+      name: "Washington",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-wa.pdf"
+    },
+    CA: {
+      name: "California",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-ca.pdf"
+    },
+    MI: {
+      name: "Michigan",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-mi.pdf"
+    },
+    ID: {
+      name: "Idaho",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-id.pdf"
+    },
+    GU: {
+      name: "Guam",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-gu.pdf"
+    },
+    VI: {
+      name: "Virgin Islands",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-11/documents/climate-change-usvi.pdf"
+    },
+    PR: {
+      name: "Puerto Rico",
+      hide: "no",
+      inactive: "no",
+      url: "https://19january2017snapshot.epa.gov/sites/production/files/2016-09/documents/climate-change-pr.pdf"
+    },
+    MP: {
+      name: "Northern Mariana Islands",
+      hide: "yes",
+      inactive: "no"
+    }
+  },
+  locations: {},
+  regions: {},
+  labels: {}
+};
